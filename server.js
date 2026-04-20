61adf93a619db32278a29f67499970e0272fae89
youssifalaa313-prog <youssifalaa313@gmail.com>
2026-04-21 00:39:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -144,4 +144,8 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("🚀 Server running on port " + PORT);
+  
+});
+app.get('/ping', (req, res) => {
+  res.send("OK");
 });
