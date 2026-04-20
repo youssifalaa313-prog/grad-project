85e599815310f1b492de6601c61e269956661601
youssifalaa313-prog <youssifalaa313@gmail.com>
2026-04-21 00:46:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -139,13 +139,14 @@ cron.schedule("*/6 * * * *", () => {
   sendToGoogleSheets();
 });
 
+/* -------- PING ROUTE -------- */
+app.get('/ping', (req, res) => {
+  res.status(200).send("OK");
+});
+
 /* -------- SERVER -------- */
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log("🚀 Server running on port " + PORT);
-  
-});
-app.get('/ping', (req, res) => {
-  res.send("OK");
 });
